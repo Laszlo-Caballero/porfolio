2ad7d237956707ba43de299f4965ad8a2c7ab5c7
laszlocaballero <laszlocaballero@gmail.com>
2025-10-05 00:51:24 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/layout/Hero/Hero.tsx
+++ src/components/layout/Hero/Hero.tsx
@@ -32,19 +32,19 @@ export default async function Hero() {
           {t('description')}
         </Typography>
         <div className="flex h-full flex-wrap items-center gap-4">
-          <ButtonLink href="a">
+          <ButtonLink href="https://github.com/Laszlo-Caballero">
             <GithubIcon className="p-xl:size-6 p-lg:size-4" />
             Github
           </ButtonLink>
-          <ButtonLink href="a">
+          <ButtonLink href="https://www.linkedin.com/in/laszlo-caballero-sipiran/">
             <LinkedlnIcon className="p-xl:size-6 p-lg:size-4" />
             Linkedln
           </ButtonLink>
-          <ButtonLink href="a">
+          <ButtonLink href="https://t.me/laszlocaballero">
             <TelegramIcon className="p-xl:size-6 p-lg:size-4" />
             Telegram
           </ButtonLink>
-          <ButtonLink href="a" className="bg-[#4F7CFF] text-white">
+          <ButtonLink href="/cv_laszlo.pdf" className="bg-[#4F7CFF] text-white">
             <DownloadIcon className="p-xl:size-6 p-lg:size-4" />
             Descargar CV
           </ButtonLink>
